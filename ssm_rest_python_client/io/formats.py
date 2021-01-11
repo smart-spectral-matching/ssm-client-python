89501aad91add0a4be14ecd10602bc6a5f3cdd52
Marshall McDonnell <mcdonnellmt@ornl.gov>
2021-01-11 10:47:19 -0500

    Re #4 - simple read and write prototypes

---
+++ ssm_rest_python_client/io/formats.py
@@ -0,0 +1,42 @@
+from importlib import import_module
+
+_MODULE_BASE='ssm_rest_python_client.io.'
+
+class UnknownFileTypeError(Exception):
+    pass
+
+ioformats = { 
+    'scidata-jsonld': 'scidata',
+    'jcamp': 'jcamp'
+}
+
+def _get_ioformat(name):
+    if name not in ioformats:
+        raise UnknownFileTypeError(name)
+    module= _MODULE_BASE + ioformats[name]
+    fmt = import_module(module)
+    return fmt
+
+def _readfunc(module, name):
+    return getattr(module, 'read_' + name)
+
+def _writefunc(module, name):
+    return getattr(module, 'write_' + name)
+
+
+def read(filename, ioformat=None, **kwargs) -> dict:
+    """
+    Read SciData dict from file format
+    """
+    module = _get_ioformat(ioformat)
+    function = _readfunc(module, ioformats.get(ioformat))
+    return function(**kwargs)
+
+def write(filename, scidata_dict, ioformat=None, **kwargs) -> None:
+    """
+    Write SciData dict to file format
+    """
+    module = _get_ioformat(ioformat)
+    function = _writefunc(module, ioformats.get(ioformat))
+    return function(**kwargs)
+
---
+++ ssm_rest_python_client/io/scidata.py
@@ -0,0 +1,5 @@
+def read_scidata(cat=None):
+    print(f'in read_scidata - cat: {cat}')
+
+def write_scidata(cat=None):
+    print(f'in write_scidata - cat: {cat}')
