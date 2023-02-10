d3f6c13ca186ae8266a8d35cc5daa99d532bb3ec
Marshall McDonnell <mcdonnellmt@ornl.gov>
2023-02-10 13:46:41 -0500

    Fix line length in main module

--- ssm_rest_python_client/__init__.py
+++ ssm_rest_python_client/__init__.py
@@ -13,7 +13,7 @@ from . import io
 try:
     __version__ = metadata.version(__package__)
 except metadata.PackageNotFoundError:
-    __version__ = toml.load("pyproject.toml")["tool"]["poetry"]["version"] + "dev"
+    __version__ = toml.load("pyproject.toml")["tool"]["poetry"]["version"] + "dev"  # noqa
 
 
 __all__ = [
